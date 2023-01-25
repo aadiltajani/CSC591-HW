a30cf25011bf381c03c091ded48dabf6c61112ca
Sukruthi Modem <121056488+sukruthimodem@users.noreply.github.com>
2023-01-25 17:28:20 -0500

    added classes.py

---
+++ src/classes.py
@@ -0,0 +1,13 @@
+class obj:
+    id = 0
+    def __init__(self, s, t=None, new=None):
+        if new is None:
+            def new(self, *args):
+                obj.id += 1
+                i = {'a': s, 'id': obj.id}
+                i.update(t.__dict__)
+                t.new(i, *args)
+                return i
+        t = type(s, (object,), {})
+        t.__call__ = new
+        self.t = t

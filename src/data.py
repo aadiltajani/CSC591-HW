e67136d70379070bab5056b8b6af0789258738fc
Sukruthi Modem <121056488+sukruthimodem@users.noreply.github.com>
2023-01-25 17:26:10 -0500

    Created data.py

---
+++ src/data.py
@@ -0,0 +1,33 @@
+class DATA:
+    def __init__(self, src=None, fun=None):
+        self.rows = []
+        self.cols = None
+        if fun is None:
+            def fun(x):
+                self.add(x)
+        if isinstance(src, str):
+            # load from csv file on disk
+            csv(src, fun)
+        else:
+            # load from list
+            for x in src or []:
+                fun(x)
+
+    def add(self, t):
+        if self.cols:
+            t = t.cells if hasattr(t, "cells") else ROW(t)
+            self.rows.append(t)
+            self.cols.add(t)
+        else:
+            self.cols = COLS(t)
+
+    def clone(self, init=None):
+        data = DATA(self.cols.names)
+        for x in init or []:
+            data.add(x)
+        return data
+
+    def stats(self, what=None, cols=None, nPlaces=None):
+        def fun(k, col):
+            return round(getattr(col, what or "mid")(col), nPlaces), col.txt
+        return {k: fun(k, col) for k, col in (cols or self.cols.y).items()}

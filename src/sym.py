e61d9fc6b72b7c1ad6bea745a65a3fb8e32a0963
urmi6899 <urmi6899@gmail.com>
2023-02-01 18:50:27 -0500

    modified sym.py

--- src/sym.py
+++ src/sym.py
@@ -31,3 +31,11 @@ class sym:
 
     def rnd(self, v, n):
         return v
+
+    def dist(self,s1,s2):
+        if s1 == '?' and s2 == '?':
+            return 1
+        elif s1==s2:
+            return 0
+        else:
+            return 1

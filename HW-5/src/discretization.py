a18cb3dd2a31fea57f57e5e8176920ed26a685a5
Sukruthi Modem <smodem@ncsu.edu>
2023-03-10 17:31:43 -0500

    changed col to dictionary

--- HW-5/src/discretization.py
+++ HW-5/src/discretization.py
@@ -7,10 +7,10 @@ def bins(cols, rowss, the):
         ranges = []
         for y, rows in enumerate(rowss):
             for row in rows:
-                x,k = row[col.at]
+                x,k = row[col['at']]
                 if x != '?':
                     k = bin(col, x, the)
-                    ranges[k] = ranges[k] or functions.range(col.at, col.txt, x)
+                    ranges[k] = ranges[k] or functions.range(col['at'], col['txt'], x)
                     functions.extend(ranges[k], x, y)
         ranges = sorted(functions.map(ranges, functions.itself))
         out[len(out)] = ranges if col.isSym else mergeAny(ranges)
@@ -19,8 +19,8 @@ def bins(cols, rowss, the):
 def bin(col, x, the):
     if x == '?' or col.isSym:
         return x
-    tmp = (col.hi - col.lo) / (the.bins - 1)
-    return 1 if col.hi == col.lo else math.floor(x/tmp + 0.5)*tmp
+    tmp = (col['hi'] - col['lo'] / (the.bins - 1))
+    return 1 if col['hi'] == col['lo'] else math.floor(x/tmp + 0.5)*tmp
 
 def noGaps(t):
     for j in range(1, len(t)):
@@ -50,11 +50,11 @@ def merge2(col1, col2):
 def merge(col1, col2):
     new = merge(col1, col2)
     if col1.isSym:
-        for x,n in enumerate(col2.has):
+        for x,n in enumerate(col2['has']):
             functions.add(new, x, n)
     else:
-        for n in col2.has:
+        for n in col2['has']:
             functions.add(new, n)
-        new.lo = min(col1.lo, col2.lo)
-        new.hi = max(col1.hi, col2.hi)
+        new['lo'] = min(col1['lo'], col2['lo'])
+        new['hi'] = max(col1['hi'], col2['hi'])
     return new
